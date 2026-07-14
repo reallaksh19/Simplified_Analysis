c1a8d61870572b068b0dd7f1d8c86711637386c1
reallaksh19 <reallaksh19@gmail.com>
2026-07-14 04:49:49 +0400

    feat: add topology workspace events

---
+++ src/workspace/topology-events.js
@@ -0,0 +1,9 @@
+export const TOPOLOGY_EVENTS = Object.freeze({
+  CHANGED: 'topology:changed',
+  REBUILD_EXACT_REQUESTED: 'topology:rebuildExactRequested',
+  REBUILD_TOLERANCE_REQUESTED: 'topology:rebuildToleranceRequested',
+  REBUILD_FAILED: 'topology:rebuildFailed',
+  EXPORT_REQUESTED: 'topology:exportRequested',
+  EXPORT_COMPLETED: 'topology:exportCompleted',
+  EXPORT_FAILED: 'topology:exportFailed',
+});
