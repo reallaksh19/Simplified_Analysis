eea6c064dba515ab02c1e2887900a2936d5d00d0
reallaksh19 <reallaksh19@gmail.com>
2026-07-14 21:16:26 +0400

    feat: add vertical beam workspace events

---
+++ src/workspace/vertical-beam-events.js
@@ -0,0 +1,10 @@
+export const VERTICAL_BEAM_EVENTS = Object.freeze({
+  CHANGED: 'verticalBeam:changed',
+  REBUILD_REQUESTED: 'verticalBeam:rebuildRequested',
+  SOLVE_REQUESTED: 'verticalBeam:solveRequested',
+  REBUILD_FAILED: 'verticalBeam:rebuildFailed',
+  SOLVE_FAILED: 'verticalBeam:solveFailed',
+  EXPORT_REQUESTED: 'verticalBeam:exportRequested',
+  EXPORT_COMPLETED: 'verticalBeam:exportCompleted',
+  EXPORT_FAILED: 'verticalBeam:exportFailed',
+});
