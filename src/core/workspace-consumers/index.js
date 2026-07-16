42796f982b9c3e8f2dc02118dd0096b3e1ac9131
reallaksh19 <reallaksh19@gmail.com>
2026-07-16 16:55:53 +0400

    feat: export W10.8 consumer contracts

---
+++ src/core/workspace-consumers/index.js
@@ -0,0 +1,29 @@
+export {
+  APPLICATION_VIEW_IDS,
+  APPLICATION_VIEW_STATE_SCHEMA,
+  CONSUMER_IDS,
+  CONTRACT_KEYS,
+  IMPLEMENTATION_STATUS,
+  READINESS_STATES,
+  WORKSPACE_CONSUMER_CONTEXT_SCHEMA,
+  WORKSPACE_CONSUMER_READINESS_SCHEMA,
+  WORKSPACE_CONSUMER_REGISTRY_SCHEMA,
+} from './constants.js';
+export { createWorkspaceConsumerContext, validateWorkspaceConsumerContext } from './context.js';
+export {
+  createWorkspaceConsumerRegistry,
+  validateWorkspaceConsumerRegistry,
+  workspaceConsumerDescriptor,
+} from './registry.js';
+export {
+  createWorkspaceConsumerReadiness,
+  createWorkspaceConsumerReadinessRegistry,
+  validateWorkspaceConsumerReadiness,
+} from './readiness.js';
+export {
+  assertApplicationViewId,
+  createApplicationViewState,
+  refreshApplicationViewState,
+  transitionApplicationViewState,
+  validateApplicationViewState,
+} from './view-state.js';
