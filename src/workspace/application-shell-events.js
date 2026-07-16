deb1f43775852af46439cd35a9722f6c8cbdd842
reallaksh19 <reallaksh19@gmail.com>
2026-07-16 20:50:21 +0400

    feat(w10.8): add closed application shell events

---
+++ src/workspace/application-shell-events.js
@@ -0,0 +1,6 @@
+export const APPLICATION_SHELL_EVENTS = Object.freeze({
+  CHANGE_REQUESTED: 'applicationView:changeRequested',
+  CHANGED: 'applicationView:changed',
+  CHANGE_FAILED: 'applicationView:changeFailed',
+  CONTEXT_CHANGED: 'workspaceConsumerContext:changed',
+});
