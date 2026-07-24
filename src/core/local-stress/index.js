e427a7eb5630d1d472329401e15ee9d8abb1a6f4
reallaksh19 <reallaksh19@gmail.com>
2026-07-24 14:02:55 +0400

    export LAFEA.1 canonical foundation API

--- src/core/local-stress/index.js
+++ src/core/local-stress/index.js
@@ -1,11 +1,15 @@
 export {
-  ACTION_SENSES,
-  ENGINEERING_LEVEL,
-  FORMULA_IDS,
-  LIMITATIONS,
-  LOCAL_STRESS_INPUT_SCHEMA,
-  LOCAL_STRESS_RESULT_SCHEMA,
-  NUMERICAL_PROFILE,
-  PRESSURE_END_CONDITIONS,
+  ACTION_SENSES, BASE_LIMITATIONS, CANONICAL_UNITS, COORDINATE_SYSTEMS,
+  END_CONDITIONS, ENGINEERING_LEVEL, FORMULA_IDS, MODEL_SCHEMA,
+  PRESSURE_LIMITATIONS, QUALIFICATION_PROFILE, QUALIFICATION_PROFILE_SCHEMA,
+  QUALIFICATION_STATES, REQUEST_TYPES, RESULT_SCHEMA, THICKNESS_POLICIES,
 } from './constants.js';
-export { calculateLocalStressFoundation } from './calculate.js';
+export {
+  createCanonicalLocalAttachmentFoundationModel,
+  validateCanonicalLocalAttachmentFoundationModel,
+} from './canonical-model.js';
+export {
+  calculateLocalAttachmentFoundation,
+  calculateLocalStressFoundation,
+  reconstructResultHashes,
+} from './calculate.js';
